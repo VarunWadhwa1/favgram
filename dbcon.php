605c065b2dedd6f81d5901bfaffd6f7f4a3fa611
Austinjohnson7039 <Austinjohnson7039@users.noreply.github.com>
2021-08-30 18:33:38 +0530

    done

--- dbcon.php
+++ dbcon.php
@@ -1,1 +1,1 @@
-	<?php $conn = new PDO('mysql:host=localhost;dbname=socialdb', 'root', ''); ?>
+	<?php $conn = new PDO('mysql:host=remotemysql.com;dbname=PgWnY9S5i0', 'PgWnY9S5i0', 'v5q1xTLg4j'); ?>
