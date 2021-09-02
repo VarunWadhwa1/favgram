bd542fbc8c9ef7380b61e543b6408eada4dfb985
Austinjohnson7039 <Austinjohnson7039@users.noreply.github.com>
2021-09-02 09:51:16 +0530

    update1.0

--- dbcon.php
+++ dbcon.php
@@ -1,1 +1,1 @@
-<?php $conn = new PDO('mysql:host=remotemysql.com;dbname=gBF41UOiBE', 'gBF41UOiBE', ' 2dmlC3EDWU'); ?>
+<?php $conn = new PDO('mysql:host=remotemysql.com;dbname=gBF41UOiBE', 'gBF41UOiBE', '2dmlC3EDWU'); ?>
