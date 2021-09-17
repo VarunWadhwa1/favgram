ecab2bced5f2fcbb6931177ae40b59fc50289e5b
Austinjohnson7039 <Austinjohnson7039@users.noreply.github.com>
2021-09-17 12:00:50 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,1 +1,1 @@
-<?php $conn = new PDO('sql6.freesqldatabase.com;dbname=sql6437874','sql6437874','8fCf41ntll'); ?>
+<?php $conn = new PDO('mysql:host=sql6.freesqldatabase.com;dbname=sql6437874','sql6437874','8fCf41ntll'); ?>
