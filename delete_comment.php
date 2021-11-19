1cdcf83d8048c3228738616dadb2396b231fdcc3
VarunWadhwa1 <94689768+VarunWadhwa1@users.noreply.github.com>
2021-11-19 22:51:52 +0530

    Update delete_comment.php

--- delete_comment.php
+++ delete_comment.php
@@ -1,8 +1,6 @@
 <?php
 	include('dbcon.php');
-
 	$get_id =  $_GET['cid'];
-	$conn ->query("DELETE FROM comments WHERE comment_id = '$get_id'");
+	$conn->query("DELETE FROM comments WHERE comment_id = '$get_id'");
 	header("Location: home.php");
-
 ?>
