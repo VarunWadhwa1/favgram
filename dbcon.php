dab07e2dd47301c90a821704dd611dde87e91089
VarunWadhwa1 <94689768+VarunWadhwa1@users.noreply.github.com>
2021-11-19 23:33:14 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,1 +1,1 @@
-<?php $conn = new PDO('mysql:host=remotemysql.com;dbname=oK0a7SN30y', 'oK0a7SN30y','x06fRU1935'); ?>
+<?php $conn = new PDO('mysql:host=remotemysql.com;dbname=oK0a7SN30y;charset=utf8mb4', 'oK0a7SN30y','x06fRU1935'); ?>
