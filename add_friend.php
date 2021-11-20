175a75ae0ea81619b2a4b6e09cf9dd67ae5e38c3
VarunWadhwa1 <94689768+VarunWadhwa1@users.noreply.github.com>
2021-11-20 20:18:22 +0530

    Update add_friend.php

--- add_friend.php
+++ add_friend.php
@@ -2,6 +2,6 @@
 <?php include('session.php'); ?>
 <?php
 	$my_friend_id = $_POST['my_friend_id'];
-	$conn ->query("insert into friends (my_id,my_friend_id) values('$session_id','$my_friend_id')");
+	$conn ->query("insert into friends (my_friend_id,my_id) values('$my_friend_id','$session_id')");
  	header('location:friends.php'); 
 ?>
