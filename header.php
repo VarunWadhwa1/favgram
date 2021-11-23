8ecde664a552e30f14dbf2034c3edfa52f297e36
VarunWadhwa1 <94689768+VarunWadhwa1@users.noreply.github.com>
2021-11-23 20:48:46 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
         <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
         <title>FAVGRAM</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="icon" href="/upload/favicon.ico" type="image/x-icon">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/bootstrap-glyphicons.css" type="text/css" rel="stylesheet">
 		<link href="css/font-awesome.css" type="text/css" rel="stylesheet">
