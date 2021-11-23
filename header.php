68f4e88ba2ac3e2f1cf088eeeba9e72a196818f6
VarunWadhwa1 <94689768+VarunWadhwa1@users.noreply.github.com>
2021-11-23 20:52:13 +0530

    Update header.php

--- header.php
+++ header.php
@@ -4,10 +4,10 @@
         <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
         <title>FAVGRAM</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <link rel="icon" href="/upload/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="../upload/favicon.png" type="image/icon type">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/bootstrap-glyphicons.css" type="text/css" rel="stylesheet">
-		<link href="css/font-awesome.css" type="text/css" rel="stylesheet">
-		<link href="css/my_style.css" type="text/css" rel="stylesheet">
-	</head>
-	<?php include('dbcon.php'); ?>
+	<link href="css/font-awesome.css" type="text/css" rel="stylesheet">
+	<link href="css/my_style.css" type="text/css" rel="stylesheet">
+    </head>
+<?php include('dbcon.php'); ?>
